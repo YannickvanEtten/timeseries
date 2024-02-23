25598e872519f54b55c0fe867f1f377a5eff485b
Yannick van Etten <yannickvanetten29@gmail.com>
2024-02-23 18:22:50 +0100

    final

--- Assignment part 1.py
+++ Assignment part 1.py
@@ -208,6 +208,226 @@ def est_params(y):
     result = minimize(loglikilihood, initial_values, args=(y,), method='L-BFGS-B')
     return result
 
+#########################
+# Plot functions
+
+def plot_kalman_filter(df,a,v,F,K,P):
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8), tight_layout=True)
+    z_95 = 1.645
+    a_lower = a - z_95 * np.sqrt(P)
+    a_upper = a + z_95 * np.sqrt(P)
+    
+    axs[0, 0].plot(df['Year'][1:],df['Nile'][1:], 'bo', markersize=2, label = 'Nile')
+    axs[0, 0].plot(df['Year'][1:], a[1:],color='black')
+    axs[0, 0].plot(df['Year'][1:], a_lower[1:],color='green')
+    axs[0, 0].plot(df['Year'][1:], a_upper[1:],color='green')
+    axs[0, 0].set_title('y_t and a_t')
+
+    axs[0, 1].plot(df['Year'][1:], P[1:])
+    axs[0, 1].set_title('P_t')
+
+    axs[1, 0].plot(df['Year'][1:], v[1:])
+    axs[1, 0].axhline(y=0, linestyle='-')
+    axs[1, 0].set_title('v_t')
+
+    axs[1, 1].plot(df['Year'][1:], F[1:])
+    axs[1, 1].set_title('F_t')
+
+    for ax in axs.flat:
+        ax.tick_params(axis='both', which='major', labelsize=12)
+
+    plt.show()
+
+def plot_kalman_smoother(df,alpha,V,r,N):
+    number_obs = len(df)
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8), tight_layout=True)
+    
+    z_95 = 1.645
+    alpha_lower = alpha - z_95 * np.sqrt(V)
+    alpha_upper = alpha + z_95 * np.sqrt(V)
+    
+    axs[0, 0].plot(df['Year'],df['Nile'], 'bo', markersize=2, label = 'Nile')
+    axs[0, 0].plot(df['Year'], alpha,color='black')
+    axs[0, 0].plot(df['Year'], alpha_lower,color='green')
+    axs[0, 0].plot(df['Year'], alpha_upper,color='green')
+    axs[0, 0].set_title('y_t and alpha_t')
+
+    axs[0, 1].plot(df['Year'], V)
+    axs[0, 1].set_title('V_t')
+
+    axs[1, 0].plot(df['Year'][:number_obs-1], r[:number_obs-1])
+    axs[1, 0].axhline(y=0, linestyle='-')
+    axs[1, 0].set_title('r_t')
+
+    axs[1, 1].plot(df['Year'][:number_obs-1], N[:number_obs-1])
+    axs[1, 1].set_title('N_t')
+
+    for ax in axs.flat:
+        ax.tick_params(axis='both', which='major', labelsize=12)
+
+    plt.show()
+
+def plot_kalman_obs_dist(df,epsilon_hat, smoothed_sd_eps, eta_hat, smoothed_sd_eta):
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8), tight_layout=True)
+  
+    axs[0, 0].plot(df['Year'], epsilon_hat)
+    axs[0, 0].axhline(y=0, linestyle='-')
+    axs[0, 0].set_title('epsilon hat')
+
+    axs[0, 1].plot(df['Year'], smoothed_sd_eps)
+    axs[0, 1].set_title('obs error var')
+
+    axs[1, 0].plot(df['Year'],eta_hat)
+    axs[1, 0].axhline(y=0, linestyle='-')
+    axs[1, 0].set_title('eta hat')
+
+    axs[1, 1].plot(df['Year'], smoothed_sd_eta)
+    axs[1, 1].set_title('state error var')
+
+    for ax in axs.flat:
+        ax.tick_params(axis='both', which='major', labelsize=12)
+
+    plt.show()
+
+def plot_simulation(df,alpha,alpha_plus,alpha_tilde,epsilon_hat,epsilon_tilde,eta_hat,eta_tilde):
+    number_obs = len(df)
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8), tight_layout=True)
+  
+    axs[0, 0].plot(df['Year'], alpha, color='black')
+    axs[0, 0].plot(df['Year'],alpha_plus, 'bo', markersize=2)
+    axs[0, 0].set_title('alpha hat and alpha plus')
+
+    axs[0, 1].plot(df['Year'], alpha, color='black')
+    axs[0, 1].plot(df['Year'],alpha_tilde, 'bo', markersize=2)
+    axs[0, 1].set_title('alpha hat and alpha tilde')
+
+    axs[1, 0].plot(df['Year'],epsilon_hat)
+    axs[1, 0].plot(df['Year'],epsilon_tilde, 'bo', markersize=2)
+    axs[1, 0].axhline(y=0, linestyle='-')
+    axs[1, 0].set_title('epsilon hat')
+
+    axs[1, 1].plot(df['Year'],eta_hat)
+    axs[1, 1].plot(df['Year'][:number_obs-1],eta_tilde, 'bo', markersize=2)
+    axs[1, 1].axhline(y=0, linestyle='-')
+    axs[1, 1].set_title('eta hat')
+
+    for ax in axs.flat:
+        ax.tick_params(axis='both', which='major', labelsize=12)
+
+    plt.show()
+
+def plot_kalman_weights(df,broken_nile,new_a,new_P,new_alpha,new_V,break_begin1,break_end1,break_begin2,break_end2):
+    number_obs = len(df)
+    
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8), tight_layout=True)
+    
+    axs[0, 0].plot(df['Year'], broken_nile, label = 'Nile')
+    axs[0, 0].plot(df['Year'], new_a,color='black')
+    axs[0, 0].set_title('data and filtered a')
+    
+    axs[0, 1].plot(df['Year'], new_P)
+    axs[0, 1].set_title('new P')
+   
+    axs[1, 0].plot(df['Year'],broken_nile, label = 'Nile')
+    axs[1, 0].plot(df['Year'], new_alpha,color='black')
+    axs[1, 0].set_title('data and filtered alpha')
+    
+    axs[1, 1].plot(df['Year'], new_V)
+    axs[1, 1].set_title('new V')
+    
+    for ax in axs.flat:
+        ax.tick_params(axis='both', which='major', labelsize=12)
+
+    plt.show()
+
+def plot_forecasting(df,forecasting_time,a_forecast,P_forecast,F_forecast,lower_bound_50,upper_bound_50):
+    number_obs = len(df)
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8), tight_layout=True)
+
+    axs[0, 0].plot(df['Year'],df['Nile'], 'bo', markersize=2)
+    axs[0, 0].plot(forecasting_time[1:], a_forecast[1:], color='black')
+    axs[0, 0].plot(forecasting_time[number_obs:], lower_bound_50, color='grey')
+    axs[0, 0].plot(forecasting_time[number_obs:], upper_bound_50, color='grey')
+    axs[0, 0].set_title('a forecast and data')
+    
+    axs[0, 1].plot(forecasting_time[1:], P_forecast[1:], color='black')
+    axs[0, 1].set_title('P forecast')
+    
+    axs[1, 0].plot(forecasting_time[1:], a_forecast[1:], color='black')
+    axs[1, 0].set_title('a forecast')
+
+    axs[1, 1].plot(forecasting_time[1:], F_forecast[1:], color='black')
+    axs[1, 1].set_title('F forecast')
+    
+    for ax in axs.flat:
+        ax.tick_params(axis='both', which='major', labelsize=12)
+
+    plt.show()
+
+def plot_standardised_errors(df,e):
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8), tight_layout=True)
+
+    axs[0, 0].plot(df['Year'],e,color='black') 
+    axs[0, 0].axhline(y=0, linestyle='-')
+    axs[0, 0].set_title('e_t')
+    
+    axs[0, 1].set_xlim(-3.8, 3.8)
+    n, bins, patches = axs[0, 1].hist(e, bins=13, density=True, color='blue',edgecolor='black')
+    x = np.linspace(-3.8, 3.8, 100)
+    p = stats.norm.pdf(x, np.mean(e), np.std(e))
+    axs[0, 1].plot(x, p, 'k', linewidth=2) 
+    axs[0, 1].set_title('Histogram e_t')
+    
+    sorted_data = np.sort(e)
+    expected_quantiles = stats.norm.ppf(np.linspace(0.01, 0.99, 100))
+    axs[1,0].plot(expected_quantiles, sorted_data)
+    axs[1,0].plot([-2.4,2.4], [-2.4,2.4], ls="--")
+    axs[1,0].set_title('QQ plot e_t')
+    
+    #sm.graphics.tsa.plot_acf(e, lags=10,alpha = 0.9, ax=axs[1, 1],marker=None) 
+    axs[1, 1].set_title('Correlogram e_t')
+
+    #To do it in blocks instead of lines
+    acf_vals = sm.tsa.acf(e, nlags=10)
+    axs[1,1].bar(np.arange(1,len(acf_vals)), acf_vals[1:], width=0.4, color='blue', alpha=0.9)
+    axs[1,1].axhline(y=0, linestyle='-')
+    axs[1,1].set_ylim(-1, 1)
+
+    for ax in axs.flat:
+        ax.tick_params(axis='both', which='major', labelsize=12)
+
+    plt.show()
+
+def plot_stand_smoothed_res(df,u_star,r_star):
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8), tight_layout=True)
+
+    axs[0, 0].plot(df['Year'],u_star,color='black') 
+    axs[0, 0].axhline(y=0, linestyle='-')
+    axs[0, 0].set_title('u_star')   
+    
+    n, bins, patches = axs[0, 1].hist(u_star, bins=13, density=True, color='blue',edgecolor='black')
+    x = np.linspace(-4, 3, 100)
+    p = stats.norm.pdf(x, np.mean(u_star), np.std(u_star))
+    axs[0, 1].plot(x, p, 'k', linewidth=2) 
+    axs[0, 1].set_title('Histogram u_star')  
+    
+    axs[1, 0].plot(df['Year'],r_star,color='black') 
+    axs[1, 0].axhline(y=0, linestyle='-')
+    axs[1, 0].set_title('r_star')
+    
+    n, bins, patches = axs[1, 1].hist(r_star, bins=13, density=True, color='blue',edgecolor='black')
+    x = np.linspace(-4, 3, 100)
+    p = stats.norm.pdf(x, np.mean(r_star), np.std(r_star))
+    axs[1, 1].plot(x, p, 'k', linewidth=2) 
+    axs[1, 1].set_title('Histogram r_star')
+
+    for ax in axs.flat:
+        ax.tick_params(axis='both', which='major', labelsize=12)
+
+    plt.show()
+
+##################
+
 def main():
     df_nile = pd.read_excel("Data/Nile.xlsx")
     df_nile.rename(columns={'Unnamed: 0': 'Year'}, inplace=True)
@@ -226,35 +446,43 @@ def main():
     
     #figure 1
     a,v,F,K,P = kalman_filter(df_nile['Nile'],sigma_sq_eta,sigma_sq_eps)
-    plots.plot_kalman_filter(df_nile,a,v,F,K,P)
+    #plots.plot_kalman_filter(df_nile,a,v,F,K,P)
+    plot_kalman_filter(df_nile,a,v,F,K,P)
     
     #figure 2
     alpha,V,r,N  = kalman_smoother(df_nile,a,v,F,K,P)
-    plots.plot_kalman_smoother(df_nile,alpha,V,r,N)
+    #plots.plot_kalman_smoother(df_nile,alpha,V,r,N)
+    plot_kalman_smoother(df_nile,alpha,V,r,N)
     
     #figure 3
     epsilon_hat, smoothed_sd_eps, eta_hat, smoothed_sd_eta,u,D = kalman_obs_disturb(df_nile,sigma_sq_eta,sigma_sq_eps,F,v,K,r,N)
-    plots.plot_kalman_obs_dist(df_nile,epsilon_hat, smoothed_sd_eps, eta_hat, smoothed_sd_eta)
+    #plots.plot_kalman_obs_dist(df_nile,epsilon_hat, smoothed_sd_eps, eta_hat, smoothed_sd_eta)
+    plot_kalman_obs_dist(df_nile,epsilon_hat, smoothed_sd_eps, eta_hat, smoothed_sd_eta)
     
     #figure 4
     alpha_hat,alpha_plus,alpha_tilde,epsilon_hat,epsilon_tilde,eta_hat,eta_tilde = simulation(df_nile['Nile'],sigma_sq_eta,sigma_sq_eps,epsilon_hat,eta_hat,epsilon_plus,eta_plus)
-    plots.plot_simulation(df_nile,alpha,alpha_plus,alpha_tilde,epsilon_hat,epsilon_tilde,eta_hat,eta_tilde)
+    #plots.plot_simulation(df_nile,alpha,alpha_plus,alpha_tilde,epsilon_hat,epsilon_tilde,eta_hat,eta_tilde)
+    plot_simulation(df_nile,alpha,alpha_plus,alpha_tilde,epsilon_hat,epsilon_tilde,eta_hat,eta_tilde)
 
     #figure 5
     broken_nile,new_a,new_P,new_alpha,new_V = kalman_weight(df_nile,sigma_sq_eta,sigma_sq_eps,break_begin1,break_end1,break_begin2,break_end2)
-    plots.plot_kalman_weights(df_nile,broken_nile,new_a,new_P,new_alpha,new_V,break_begin1,break_end1,break_begin2,break_end2)
+    #plots.plot_kalman_weights(df_nile,broken_nile,new_a,new_P,new_alpha,new_V,break_begin1,break_end1,break_begin2,break_end2)
+    plot_kalman_weights(df_nile,broken_nile,new_a,new_P,new_alpha,new_V,break_begin1,break_end1,break_begin2,break_end2)
 
     #figure 6
     forecasting_time,a_forecast,P_forecast,F_forecast,lower_bound,upper_bound = forecasting(df_nile,a,P,F,K,sigma_sq_eps,sigma_sq_eta)
-    plots.plot_forecasting(df_nile,forecasting_time,a_forecast,P_forecast,F_forecast,lower_bound,upper_bound)
-    
+    #plots.plot_forecasting(df_nile,forecasting_time,a_forecast,P_forecast,F_forecast,lower_bound,upper_bound)
+    plot_forecasting(df_nile,forecasting_time,a_forecast,P_forecast,F_forecast,lower_bound,upper_bound)
+
     #figure 7
     e = standardised_errors(df_nile,v,F)
-    plots.plot_standardised_errors(df_nile,e)
+    #plots.plot_standardised_errors(df_nile,e)
+    plot_standardised_errors(df_nile,e)
     
     #figure 8
     u_star, r_star = stand_smoothed_res(u,D,r,N)
-    plots.plot_stand_smoothed_res(df_nile,u_star,r_star)
+    #plots.plot_stand_smoothed_res(df_nile,u_star,r_star)
+    plot_stand_smoothed_res(df_nile,u_star,r_star)
     
     res = est_params(df_nile['Nile'])
     print(res)
